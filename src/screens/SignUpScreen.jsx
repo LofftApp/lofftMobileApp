08eae4eecf95de3745a0d1c3cc7189572041cef0
Donghwan Kim <donghwan.kim92@gmail.com>
2022-10-23 12:40:29 +0200

    created the signup screen

---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+const SignUpScreen = () => {
+  return (
+    <View>
+      <Text>This is the Sign up screen</Text>
+    </View>
+  );
+};
+
+export default SignUpScreen;
