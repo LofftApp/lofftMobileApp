fbbaf709ee1efae39d86d44394ac5508292aee73
Donghwan Kim <donghwan.kim92@gmail.com>
2022-10-23 12:40:29 +0200

    created the signup screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {navigationRef} from './src/RootNavigation';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import AnotherScreen from './src/screens/AnotherScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
     <Stack.Navigator initialRouteName="WelcomeScreen">
       <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
       <Stack.Screen name="AnotherScreen" component={AnotherScreen} />
+      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
     </Stack.Navigator>
   );
 };
