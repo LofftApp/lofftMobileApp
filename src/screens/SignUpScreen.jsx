2063542ba0efe7f9dae50bbab2d796473f717ae4
Donghwan Kim <donghwan.kim92@gmail.com>
2022-10-23 23:53:44 +0200

    checkbox installed

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 
 const SignUpScreen = () => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+
   return (
     <View>
-      <Text>This is the Sign up screen</Text>
+      <View></View>
+      <View>
+        <View>
+          <Text>Create account</Text>
+          <TextInput placeholder="Email"></TextInput>
+          <TextInput placeholder="Password"></TextInput>
+          <TextInput placeholder="Repeat Password"></TextInput>
+          <CheckBox
+            disabled={false}
+            value={toggleCheckBox}
+            onValueChange={newValue => setToggleCheckBox(newValue)}
+          />
+          <Text>I agree to...</Text>
+        </View>
+        <Button title="Sign Up"></Button>
+        <View>
+          <Text>Or sign in with</Text>
+        </View>
+      </View>
     </View>
   );
 };
