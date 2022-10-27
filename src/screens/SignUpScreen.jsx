ce0b1ad1904e770d0cd712250a36c9cb87621ab6
Donghwan Kim <donghwan.kim92@gmail.com>
2022-10-27 20:10:23 +0200

    page layout added

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -2,26 +2,35 @@ import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import SignUpForm from '../components/SignUpForm';
 import SignInWith from '../components/SignInWith';
+import Color from '../styles/lofftColorPallet.json';
 
 const SignUpScreen = () => {
   return (
-    <View style={styles.mainView}>
-      <View style={{flex: 1}}></View>
-      <SignUpForm style={{flex: 2}} />
-      {/* <SignInWith /> */}
+    <View style={styles.container}>
+      <View style={styles.illustration}></View>
+      <View style={styles.signUpForm}>
+        <SignUpForm />
+      </View>
+      <View style={styles.signInWith}>
+        <SignInWith />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  mainView: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'yellow',
+  container: {
+    flex: 1,
+    backgroundColor: Color.Lavendar['5'],
   },
-  FormView: {
-    position: 'absolute',
-    top: '100px',
+  illustration: {
+    flex: 1,
+  },
+  signUpForm: {
+    flex: 2,
+  },
+  signInWith: {
+    flex: 1,
   },
 });
 
