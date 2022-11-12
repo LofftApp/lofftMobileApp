8f6d14aecee8d4797c7d99309ae76e46828d4c99
Donghwan Kim <donghwan.kim92@gmail.com>
2022-11-12 15:12:50 +0100

    bouncy checkbox installed. Previous checkbox library deleted

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
+import Input from './Input';
+import Color from '../styles/lofftColorPallet.json';
 
 const SignUpForm = () => {
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
@@ -9,17 +11,18 @@ const SignUpForm = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Create account</Text>
       <View style={styles.textInputWrap}>
-        <TextInput style={styles.textInput} placeholder="Email"></TextInput>
-        <TextInput style={styles.textInput} placeholder="Password"></TextInput>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Repeat Password"></TextInput>
-        <CheckBox
-          disabled={false}
-          value={toggleCheckBox}
-          onValueChange={newValue => setToggleCheckBox(newValue)}
-        />
-        <Text>I agree to...</Text>
+        <Input props="Email"></Input>
+        <Input props="Create password"></Input>
+        <Input props="Repeat password"></Input>
+        <View style={styles.checkBoxWrap}>
+          <CheckBox
+            disabled={false}
+            value={toggleCheckBox}
+            style={styles.checkBox}
+            onValueChange={newValue => setToggleCheckBox(newValue)}
+          />
+          <Text>I agree to...</Text>
+        </View>
       </View>
       <Button style={styles.button} title="Sign Up"></Button>
     </View>
@@ -36,23 +39,26 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: '600',
     lineHeight: 28,
+    paddingBottom: 20,
   },
   textInputWrap: {
     width: '100%',
     borderColor: 'black',
-    borderWidth: 1,
-    justifyContent: 'space-between',
+    // borderWidth: 1,
     // flex: 2,
   },
-  textInput: {
-    width: '100%',
-    height: '15%',
-    borderColor: 'black',
-    borderWidth: 1,
-    marginTop: 10,
-  },
   button: {
-    flex: 1,
+    // flex: 1,
+  },
+  checkBoxWrap: {
+    flexDirection: 'row',
+  },
+  checkBox: {
+    // width: 50,
+    // height: 50,
+    boxType: 'BEMBoxTypeSquare',
+    tintColor: Color.Lavendar['100'],
+    margin: 10,
   },
 });
 
