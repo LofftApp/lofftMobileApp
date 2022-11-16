4e7a9f43922987c001f9c0b2096d6885caec0c09
James Hibbeard <jhibbeard@gmail.com>
2022-11-16 21:10:11 +0100

    Cleaned up and rebased

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Image, Text} from 'react-native';
 import SignUpForm from '../components/SignUpForm';
 import SignInWith from '../components/SignInWith';
