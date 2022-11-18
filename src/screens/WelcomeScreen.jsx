5f74d17425be16647af2fb19b04f5444c61693fc
James Hibbeard <jhibbeard@gmail.com>
2022-11-18 17:51:42 +0100

    Added search component

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Text, StyleSheet} from 'react-native';
 import ScreenBackButton from '../components/coreComponents/CoreScreens/ScreenBackButton';
 import {fontStyles} from '../styles/fontStyles';
-
+import InputFieldText from '../components/coreComponents/inputField/InputFieldText';
 const WelcomeScreen = ({navigation}) => {
   return (
     <ScreenBackButton navigation={navigation} title="Back Button">
       <Text style={fontStyles.headerDisplay}>
         This is the Screen with a back button
       </Text>
+      <InputFieldText placeholder="Test" />
+      <InputFieldText placeholder="Test2" />
     </ScreenBackButton>
   );
 };
