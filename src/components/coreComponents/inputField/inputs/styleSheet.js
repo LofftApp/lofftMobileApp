ad5216174fe4d4fb8ed5ccefb3abd04831d809ca
James Hibbeard <jhibbeard@gmail.com>
2022-11-19 21:09:10 +0100

    added input field as per design system, split the content into three options

---
+++ src/components/coreComponents/inputField/inputs/styleSheet.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputFieldTextStyle: {
+    margin: 0,
+    marginLeft: 11,
+    paddingVertical: 8,
+    flex: 1,
+  },
+});
