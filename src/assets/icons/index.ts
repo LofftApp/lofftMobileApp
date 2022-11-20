2ebf1f0390494650f06735bdc1fdc19648bb5555
Donghwan Kim <donghwan.kim92@gmail.com>
2022-11-20 18:14:16 +0100

    checkbox icon added

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,6 @@
 import AppleIcon from './Apple-icon.svg';
 import GoogleIcon from './Google-icon.svg';
+import CrossIcon from './Cross-icon.svg';
 export {AppleIcon};
 export {GoogleIcon};
+export {CrossIcon};
