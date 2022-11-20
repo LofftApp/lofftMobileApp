98d266feb16011798da861567fbb36fc8aeb904c
Donghwan Kim <donghwan.kim92@gmail.com>
2022-11-20 15:27:15 +0100

    checkbox applied

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,14 +1,10 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-// import Input from './Input';
 import Color from '../styles/lofftColorPallet.json';
-import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import SignUpButton from './coreComponents/buttons/SignUpButton';
 import InputFieldText from '../components/coreComponents/inputField/InputFieldText';
 
 const SignInForm = () => {
-  const [toggleCheckBox, setToggleCheckBox] = useState(false);
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello again!</Text>
@@ -44,7 +40,7 @@ const styles = StyleSheet.create({
     fontSize: 13,
     paddingTop: 15,
     paddingHorizontal: 10,
-    // alignItems: 'flex-end',
+    alignSelf: 'flex-end',
   },
   signUpButtonView: {
     width: '100%',
