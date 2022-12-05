5ba336c82c436f2f605a19e77c18fb883d891993
James Hibbeard <jhibbeard@gmail.com>
2022-12-05 01:09:17 +0100

    Added icon buttons

---
+++ src/components/coreComponents/buttons/IconButton.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {fontStyles} from '../../../styles/fontStyles';
+import Color from '../../../styles/lofftColorPallet.json';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const IconButton = ({text, icon, iconSize = 20}) => {
+  return (
+    <TouchableOpacity style={styles.buttonBorder}>
+      <View style={styles.content}>
+        <Icon name={icon} size={iconSize} />
+        <Text style={[fontStyles.headerSmall, styles.text]}>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonBorder: {
+    borderColor: Color.Black[100],
+    borderWidth: 2,
+    borderRadius: 12,
+    marginVertical: 8,
+  },
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 25,
+  },
+  text: {
+    marginLeft: 15,
+  },
+});
+
+export default IconButton;
