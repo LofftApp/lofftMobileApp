feb96710ee3a299476d3149219892e03903b1d3d
James Hibbeard <jhibbeard@gmail.com>
2022-12-05 01:09:17 +0100

    Added icon buttons

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -1,28 +1,43 @@
 import React, {useState} from 'react';
-import {Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import PrimaryScreen from '../components/coreComponents/CoreScreens/PrimaryScreen';
+import Color from '../styles/lofftColorPallet.json';
 import {fontStyles} from '../styles/fontStyles';
-import InputFieldText from '../components/coreComponents/inputField/InputFieldText';
-import CheckBox from '../components/coreComponents/interactiveElements/CheckBox';
-import CustomSwitch from '../components/coreComponents/interactiveElements/CustomSwitch';
-import auth from '@react-native-firebase/auth';
+import Icon from 'react-native-vector-icons/Ionicons';
+import IconButton from '../components/coreComponents/buttons/IconButton';
 
 const WelcomeScreen = ({navigation}) => {
-  const [checkbox, setCheckBox] = useState(true);
-  const [switchV, setSwitchV] = useState(false);
-  const handleSignOut = () => {
-    auth()
-      .signOut()
-      .then(() => console.log('User signed out!'));
-  };
-
   return (
-    <PrimaryScreen navigation={navigation} title="Back Button">
-      <Text>Hello</Text>
+    <PrimaryScreen navigation={navigation}>
+      <Text style={[fontStyles.headerDisplay, styles.header]}>
+        What brings you here?
+      </Text>
+      <Text style={[fontStyles.bodySmall, styles.textColor]}>
+        Tell us what you want to do on Lofft and we will create the matching
+        experience!
+      </Text>
+      <IconButton
+        text="I'm looking for a flat"
+        icon="search-outline"
+        onPress={() => {}}
+      />
+      <IconButton
+        text="I have a room to rent"
+        icon="home-outline"
+        onPress={() => {}}
+      />
     </PrimaryScreen>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 120,
+  },
+  textColor: {
+    color: Color.Black[80],
+    marginTop: 15,
+  },
+});
 
 export default WelcomeScreen;
