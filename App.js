08f61e177420799d9da3ac30baaf67fd88a51978
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2022-12-07 20:35:16 +0100

    test commit

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ const App = () => {
       console.log('FireStore Development Environment');
       let host = 'localhost';
       // If using Mobile device set the host as local IP
-      host = '192.168.1.34';
+      // host = '192.168.1.34';
       if (host === 'localhost') {
         console.log('Host running on local host');
       } else {
