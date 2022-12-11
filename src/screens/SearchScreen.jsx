c15f54459466ca94500ea385b9315ac17439a097
Donghwan Kim <donghwan.kim92@gmail.com>
2022-12-12 00:37:01 +0100

    search screen layout

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
 import {View, StyleSheet, Image, Text} from 'react-native';
 import ScreenBackButton from '../components/coreComponents/CoreScreens/ScreenBackButton';
+{
+  /* <ScreenBackButton nav={() => navigation.goBack()}></ScreenBackButton> */
+}
 
 const SearchScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <ScreenBackButton nav={() => navigation.goBack()}></ScreenBackButton>
-      <Text>This is Search screen</Text>
+      <View style={styles.searchOverlay}></View>
+      <View style={styles.viewTabs}></View>
+      <View style={styles.listView}></View>
+      <View style={styles.tabBar}></View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    borderWidth: 2,
+    borderColor: 'red',
     flex: 1,
+    backgroundColor: 'grey',
+    marginTop: 60,
+  },
+  searchOverlay: {
+    borderWidth: 2,
+    borderColor: 'red',
+    paddingHorizontal: 16,
+    height: 64,
+    paddingVertical: 8,
+  },
+  viewTabs: {
+    borderWidth: 2,
+    borderColor: 'red',
+    paddingHorizontal: 16,
+    height: 54,
+  },
+  listView: {
+    borderWidth: 2,
+    borderColor: 'red',
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  tabBar: {
+    borderWidth: 2,
+    borderColor: 'red',
+    paddingHorizontal: 16,
+    height: 84,
   },
 });
 
