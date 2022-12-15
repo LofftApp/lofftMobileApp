1f33ab84a2acfba9da52630c53e961b0dd19fdfe
James Hibbeard <jhibbeard@gmail.com>
2022-12-15 15:00:43 +0100

    refactored startjourney screen, and changed active button toggle

---
+++ src/screens/userJourneyScreens/lessorJourney/WhereIsFlatScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const WhereIsFlatScreen = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default WhereIsFlatScreen;
