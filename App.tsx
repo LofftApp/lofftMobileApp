3e2199d9059e498ff70eb29a8eee0307ce6ec303
James Hibbeard <jhibbeard@gmail.com>
2022-12-21 23:10:52 +0000

    Added flat details screen and redux

--- App.tsx
+++ App.tsx
@@ -30,6 +30,7 @@ import UserConditionsScreen from '@Screens/UserConditionsScreen';
 
 // Lessor Journey
 import WhereIsFlatScreen from '@Screens/userJourneyScreens/lessorJourney/WhereIsFlatScreen';
+import FlatLengthAvailableScreen from '@Screens/userJourneyScreens/lessorJourney/FlatLengthAvailableScreen';
 
 // User Journey Finder
 import FlatListScreen from './src/screens/renterFlatFindScreens/FlatListScreen';
@@ -186,6 +187,11 @@ const App = () => {
               <Stack.Screen
                 name="WhereIsFlatScreen"
                 component={WhereIsFlatScreen}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="FlatLengthAvailableScreen"
+                component={FlatLengthAvailableScreen}
                 options={{headerShown: false}}
               />
             </>
