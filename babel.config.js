efeea80cf31024019e559911654ff1a279536ed8
James Hibbeard <jhibbeard@gmail.com>
2022-12-23 23:24:36 +0000

    added env files and tokens

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+const {AppleIcon} = require('assets');
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+    ],
+  };
 };
