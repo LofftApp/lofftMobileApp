b2dc8f272fad84b890de5ef832e5358909264f52
James Hibbeard <ik8tip77c@mozmail.com>
2023-01-27 16:51:42 +0100

    Fixed typo in firebase ip

--- App.tsx
+++ App.tsx
@@ -104,7 +104,7 @@ const App = () => {
       console.log('FireStore Development Environment');
       let host = 'localhost';
       // If using Mobile device set the host as local IP
-      host = '127.0.0.17';
+      host = '127.0.0.1';
       if (host === 'localhost') {
         console.log('Host running on local host');
       } else {
