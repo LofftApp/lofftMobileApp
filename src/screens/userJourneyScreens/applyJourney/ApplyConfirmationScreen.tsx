7875767fbcc9930222264019a1a3db5833d14eed
Donghwan Kim <donghwan.kim92@gmail.com>
2023-03-20 17:41:27 +0100

    first commit

---
+++ src/screens/userJourneyScreens/applyJourney/ApplyConfirmationScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ApplyConfirmationScreen = () => {
+  return (
+    <View>
+      <Text>ApplyConfirmationScreen</Text>
+    </View>
+  );
+};
+
+export default ApplyConfirmationScreen;
