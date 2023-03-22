97fa229f24093f952af5892450e45db0c5511f0a
James Hibbeard <jhibbeard@gmail.com>
2023-03-22 19:16:16 +0100

    added foundation for flatmap redux component and initial setup@

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import userJourneyReducer from '@Redux/registration/userJourneySlice';
 import imageUploadReducer from '@Redux/imageHandling/userImageUploadSlice';
 import userReducer from '@Redux/user/usersSlice';
+import flatsSlice from '@Redux/flat/flatsSlice';
 
 export default configureStore({
   reducer: {
     userDetails: userJourneyReducer,
     imageUpload: imageUploadReducer,
     user: userReducer,
+    flats: flatsSlice,
   },
 });
