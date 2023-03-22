fc1c505ad0a7d28e1a8840e1161e4763a07d7bca
James Hibbeard <jhibbeard@gmail.com>
2023-03-22 12:02:00 +0100

    🔥 refactor(index.js): remove unused import and commented code 🎉 feat(user.selector.ts): add selectCurrentUser selector The unused import and commented code were removed during the refactor. The selectCurrentUser selector was added to the user slice of the store to allow for easy access to the current user object.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-// import store from './src/app/store'; Removed during refactor
-import store from './src/store/store';
+import store from './src/app/store'; // Removed during refactor
+// import store from './src/store/store';
 import {Provider} from 'react-redux';
 
 export default function Main() {
