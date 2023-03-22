636f4a16c5e8084a6c671486fda7508a8956bec2
James Hibbeard <jhibbeard@gmail.com>
2023-03-22 11:03:51 +0100

    🔥 refactor(index.js): remove unused import and update store import path 🔨 refactor(store.ts): add logger middleware and compose enhancers ✨ feat(user.reducer.ts): add type to action parameter The unused import of the store module was removed, and the import path was updated to reflect the new location of the store module. The logger middleware was added to the store, and the enhancers were composed to include the middleware. The action parameter in the userReducer function was given a type to improve type safety.

--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
+export const userReducer = (state = INITIAL_STATE, action: any) => {
   const {type, payload} = action;
   switch (type) {
     case USER_ACTION_TYPES.SET_CURRENT_USER:
