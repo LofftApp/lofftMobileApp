11cbaed9830156229713b45957d695504c0caea4
James Hibbeard <jhibbeard@gmail.com>
2023-03-22 11:03:51 +0100

    🔥 refactor(index.js): remove unused import and update store import path 🔨 refactor(store.ts): add logger middleware and compose enhancers ✨ feat(user.reducer.ts): add type to action parameter The unused import of the store module was removed, and the import path was updated to reflect the new location of the store module. The logger middleware was added to the store, and the enhancers were composed to include the middleware. The action parameter in the userReducer function was given a type to improve type safety.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,9 @@
 import {compose, createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
-import {rootReducer} from './reducers';
+import {rootReducer} from './root-reducer';
 
-export const store = createStore(rootReducer);
+const middlewares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
