5bd05ff7efed85dabad72605030ad08224773d8a
Donghwan Kim <donghwan.kim92@gmail.com>
2023-03-25 18:41:45 +0100

    Completed application success page

--- src/assets/background/index.ts
+++ src/assets/background/index.ts
@@ -1,4 +1,6 @@
 import SignInBackground from './signin.svg';
 import SignUpBackground from './signup.svg';
+import ApplyForFlatScreenBackground from './apply-for-flat-screen.svg';
 export {SignInBackground};
 export {SignUpBackground};
+export {ApplyForFlatScreenBackground};
