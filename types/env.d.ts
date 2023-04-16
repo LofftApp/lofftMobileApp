ec1d8a1691e59bc89447ca3471531fe0414954ce
James Hibbeard <jhibbeard@gmail.com>
2023-04-16 15:25:17 +0200

    Added type reference to the env.d.ts file

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
   export const MAPBOX_API_KEY: string;
-  export const NOTION_API_KEY: string;
+  export const LOFFT_API_CLIENT_SECRET: string;
+  export const LOFFT_API_CLIENT_SECRET: string;
 }
