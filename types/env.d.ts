d715984f54b91e77ff223159172c66414c1d20cc
James Hibbeard <jhibbeard@gmail.com>
2023-04-16 16:16:18 +0200

    🔨 refactor(App.tsx): remove unused code and console logs This commit removes unused code and console logs from the App.tsx file. Specifically, the `token` state variable and the `currentUser` dispatch call in the `useEffect` hook have been removed. Additionally, console logs have been removed from the `myState` and `userType` variables.
    🐛 fix(advertMiddleware.ts): add Bearer prefix to Authorization header
    The Authorization header was not properly formatted, which caused authentication issues. The fix adds the Bearer prefix to the token to properly format the header.
    
    🔒 chore(authenticationMiddleware.ts): add client_id and client_secret to authentication requests
    The client_id and client_secret are now included in the authentication requests to the server. This is necessary for the server to authenticate the client and grant access to the requested resources.
    
    🐛 fix(authenticationMiddleware.ts): change signOut endpoint URL and use POST instead of DELETE
    ✨ feat(authenticationMiddleware.ts): add client_id and client_secret to signOut request
    The signOut endpoint URL has been changed to 'http://localhost:3000/oauth/revoke' to match the OAuth2 specification. The request method has been changed from DELETE to POST to comply with the OAuth2 specification. The client_id and client_secret have been added to the request body to ensure that the request is authenticated.
    
    🐛 fix(authenticationSlice.ts): make userType nullable
    🔧 chore(env.d.ts): fix LOFFT_API_CLIENT_ID typo
    The userType property in the AuthenticationState interface is now nullable, which allows for a more accurate representation of the initial state. The LOFFT_API_CLIENT_ID constant in the env.d.ts file has been fixed to remove the duplicate LOFFT_API_CLIENT_SECRET constant.

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,5 @@
 declare module '@env' {
   export const MAPBOX_API_KEY: string;
   export const LOFFT_API_CLIENT_SECRET: string;
-  export const LOFFT_API_CLIENT_SECRET: string;
+  export const LOFFT_API_CLIENT_ID: string;
 }
