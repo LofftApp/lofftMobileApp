4d286e2f9cc640d3b53c4110bd6b79545ecae59a
James Hibbeard <jhibbeard@gmail.com>
2023-04-18 14:22:26 +0200

    🔨 refactor(index.js): remove unused import and variable 🔨 refactor(authenticationMiddleware.ts): remove console.log statements ✨ feat(authenticationMiddleware.ts): add clearPersister function to clear persisted data The unused import and variable were removed from index.js. The console.log statements were removed from authenticationMiddleware.ts. The clearPersister function was added to clear persisted data when the user signs out. A new package.json file was added to the persistance folder.
    🚀 chore(tsconfig.json): add path alias for persistance
    🎨 style(FlatShowScreen.tsx): remove console.log statement
    🔧 fix(persister.ts): add clearPersister function
    This commit adds a path alias for persistance in tsconfig.json, which makes it easier to import persistance-related files. The console.log statement in FlatShowScreen.tsx has been removed to improve code cleanliness. The persister module now exports a clearPersister function, which can be used to clear the persisted state.

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {persister} from '@Persistance/persister';
 import {store} from '@ReduxCore/store';
-import {persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
 
-let persister = persistStore(store);
 export default function Main() {
   return (
     <Provider store={store}>
