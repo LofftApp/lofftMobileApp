65b95bdd8910440791b8bcf6f2891c36bca191cb
Joshua Smith <sarjsmithde@gmail.com>
2023-05-08 19:59:22 +0200

    feat(ApplicantsIndexScreen.tsx): add ApplicantsCard component to display applicants fix(StatusBarComponent.tsx): add navigation to applicantsOverview screen fix(FlatInfoContainer.tsx): remove console.log statement fix(dateFormatConverter.ts): remove console.log statement

--- src/components/containers/FlatInfoContainer.tsx
+++ src/components/containers/FlatInfoContainer.tsx
@@ -46,7 +46,6 @@ const FlatInfoContainer = ({
   const expander = () => {
     setDescriptionExpansion(!descriptionExpanded);
   };
-  console.log(advert, 'advert');
 
   return (
     <View style={styles.centralizerContainer}>
