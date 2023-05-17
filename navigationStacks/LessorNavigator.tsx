e0bb66468ab5328685a0786733cb53abd021faa5
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2023-05-17 19:25:11 +0200

    added navigation/screen to statusbar for lessor

--- navigationStacks/LessorNavigator.tsx
+++ navigationStacks/LessorNavigator.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import ApplicantsIndexScreen from '@Screens/dashboard/landlord/ApplicantsIndexScreen';
+// import ApplicantsIndexScreen from '@Screens/dashboard/landlord/ApplicantsIndexScreen';
 import ApplicationShowScreen from '@Screens/dashboard/renter/ApplicationShowScreen';
 import LessorIndexScreen from '@Screens/dashboard/landlord/LessorIndexScreen';
 import SeeApplicantsScreen from '@Screens/dashboard/landlord/SeeApplicantsScreen';
 import LessorActionScreen from '@Screens/dashboard/landlord/LessorActionScreen';
+import ShortListApplicantsScreen from '@Screens/dashboard/landlord/ShortListApplicantsScreen';
+import ChatScreen from '@Screens/dashboard/landlord/ChatScreen';
+import LessorAcceptScreen from '@Screens/dashboard/landlord/LessorAcceptScreen';
 
 const Lessor = createNativeStackNavigator();
 const LessorNavigator = () => {
@@ -18,6 +21,10 @@ const LessorNavigator = () => {
       <Lessor.Screen name="LessorAction" component={LessorActionScreen} />
       <Lessor.Screen name="applicationshow" component={ApplicationShowScreen} />
       <Lessor.Screen name="Seeapplicants" component={SeeApplicantsScreen} />
+      <Lessor.Screen name="Seeprofiles" component={ShortListApplicantsScreen} />
+      <Lessor.Screen name="Gotochat💭" component={ChatScreen} />
+      <Lessor.Screen name="Finalizeit" component={LessorAcceptScreen} />
+
     </Lessor.Navigator>
   );
 };
