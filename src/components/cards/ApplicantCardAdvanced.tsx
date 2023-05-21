a4a474bf05bee6f6318dd00e0211042c26460aa3
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2023-05-21 22:49:49 +0200

    added dynamic selected buttont that count the amount of selected applicants and sets the state

--- src/components/cards/ApplicantCardAdvanced.tsx
+++ src/components/cards/ApplicantCardAdvanced.tsx
@@ -1,37 +1,87 @@
 import React, {useState, useCallback} from 'react';
-import {View, Text, StyleSheet, Pressable} from 'react-native';
+import {View, Text, StyleSheet, Pressable, Image} from 'react-native';
 
 // Styles
 import Color from '@StyleSheets/lofftColorPallet.json';
 import {fontStyles} from '@StyleSheets/fontStyles';
 
+// Icons
+import LofftIcon from '@Components/lofftIcons/LofftIcon';
+
 // External
 import CheckBox from '@Components/coreComponents/interactiveElements/CheckBox';
 
-const ApplicantsCardAdvanced = ({name, match, image, id, selectProfile}: any) => {
+const ApplicantsCardAdvanced = ({
+  name,
+  match,
+  image,
+  id,
+  selectProfile,
+}: any) => {
+
   const [activateBox, setActiveBox] = useState(false);
+
   return (
     <View style={styles.cardContainer}>
-      <Text>{name}</Text>
-      <CheckBox
-        value={activateBox}
-        onPress={() => {
-          setActiveBox(!activateBox);
-          selectProfile(id);
-        }}
-      />
+      <View style={styles.cardInnerWrapper}>
+        <CheckBox
+          value={activateBox}
+          onPress={() => {
+            setActiveBox(!activateBox);
+            selectProfile(id);
+          }}
+        />
+
+        <Image
+          source={{
+            uri: `${image}`,
+          }}
+          style={styles.avatar}
+        />
+        <View>
+          <Text>{name}</Text>
+          <Text>{match}</Text>
+        </View>
+
+        <LofftIcon
+          style={styles.icon}
+          name={'chevron-right'}
+          size={28}
+          color={Color.Blue[100]}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   cardContainer: {
-    height: '17%',
+    height: '14%',
     marginTop: 10,
     backgroundColor: Color.Lavendar[10],
-    marginHorizontal: 10,
+    marginHorizontal: 18,
     borderRadius: 12,
+    alignItems: 'center',
+    width: '90%'
   },
+  cardInnerWrapper: {
+    width: '90%',
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  avatar: {
+    height: '80%',
+    width: '32%',
+    borderRadius: 8,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  icon: {
+    position: 'absolute',
+    right: 0,
+  }
 });
 
 export default ApplicantsCardAdvanced;
