689e707e34d383b22008ea5a459c59bfb1e74eaf
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2023-09-25 16:13:18 +0200

    Created the dynamic navigator for applications shortlist screen

---
+++ src/screens/dashboard/landlord/SubScreens/SeeApplicantsScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native"
+
+const SeeApplicantsScreen = () => {
+  return (
+    <View>
+      <Text>Hello from Applicants Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default SeeApplicantsScreen;
