598a304e41b0330ff746ce5d191e859fec10f580
James Hibbeard <jhibbeard@gmail.com>
2024-05-16 00:14:17 +0200

    updated custom routes path

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      'module-resolver',
+      {
+        alias: {
+          api: './src/api',
+        },
+      },
+    ],
+    [
       'module:react-native-dotenv',
       {
         moduleName: '@env',
