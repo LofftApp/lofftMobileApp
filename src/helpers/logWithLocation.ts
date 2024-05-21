e74c413ce72c1fdccb5d7a5a5fb2b309cc775826
Jose Copeti <jrcopeti@gmail.com>
2024-05-21 17:33:14 +0200

    log with location

---
+++ src/helpers/logWithLocation.ts
@@ -0,0 +1,14 @@
+export const logWithLocation = (...messages: string[]) => {
+  const err = new Error();
+  const stack = err.stack ? err.stack.split('\n') : [];
+
+  let caller = stack[2] || stack[1] || stack[0];
+
+  if (caller) {
+    caller = caller.trim();
+  } else {
+    caller = 'unknown location';
+  }
+
+  console.log(`${caller}:`, ...messages);
+};
