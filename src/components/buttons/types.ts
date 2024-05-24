ab4ba2e1aba00fff46873eb4d1d9050d739cb9bc
Jose Copeti <jrcopeti@gmail.com>
2024-05-24 15:20:43 +0200

    type in PaginationBar and CoreButton

--- src/components/buttons/types.ts
+++ src/components/buttons/types.ts
@@ -1,3 +1,5 @@
+import {GestureResponderEvent, StyleProp, ViewStyle} from 'react-native';
+
 type HeaderPageContentSwitchProps = {
   toggleNames: string[];
   toggleIcons: string[];
@@ -6,4 +8,13 @@ type HeaderPageContentSwitchProps = {
   markers: string[];
 };
 
-export type {HeaderPageContentSwitchProps};
+type CoreButtonProps = {
+  value: string;
+  invert?: boolean;
+  style?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<ViewStyle>;
+  onPress?: (event: GestureResponderEvent) => void | undefined;
+  disabled?: boolean;
+};
+
+export type {HeaderPageContentSwitchProps, CoreButtonProps};
