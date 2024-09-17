5b355e8cc736c4d9413de1edcc318fac77af6b05
James Hibbeard <jhibbeard@gmail.com>
2024-09-17 10:26:53 +0100

    Added persister to detect if in test environment

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -10,8 +10,31 @@ import App from '../App';
 import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+// Mock the async thunk
+jest.mock('../src/features/authentication/authenticationSlice', () => ({
+  checkToken: {
+    pending: 'checkToken/pending',
+    fulfilled: 'checkToken/fulfilled',
+    rejected: 'checkToken/rejected',
+  },
+}));
+
+// Create mock store
+const mockStore = configureStore([thunk]);
+const store = mockStore({
+  authentication: {loading: false},
+});
+
+test('renders correctly', () => {
+  const {toJSON} = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  expect(toJSON()).toMatchSnapshot();
 });
