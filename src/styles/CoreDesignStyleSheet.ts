daaaa71cdfd7841368ea29de032c90a51fe4ff54
Jose Copeti <jrcopeti@gmail.com>
2024-09-25 12:19:04 +0200

    styles in CompleteProfilePopUpModal

--- src/styles/CoreDesignStyleSheet.ts
+++ src/styles/CoreDesignStyleSheet.ts
@@ -1,14 +1,22 @@
 import {StyleSheet} from 'react-native';
+import * as Color from 'styleSheets/lofftColorPallet.json';
 import * as color from './lofftColorPallet.json';
+import {size} from 'react-native-responsive-sizes';
 
 export const CoreStyleSheet = StyleSheet.create({
   viewContainerStyle: {
     backgroundColor: color.White[100],
     flex: 1,
-    paddingHorizontal: 15,
-    paddingTop: 5,
+    paddingHorizontal: size(15),
+    paddingTop: size(5),
   },
   viewContainerIOSStyle: {
     paddingTop: 35,
+  },
+  modalContainer: {
+    height: '70%',
+    marginTop: 'auto',
+    backgroundColor: Color.White[100],
+    borderRadius: 10,
   },
 });
