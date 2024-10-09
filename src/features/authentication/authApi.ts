f29498d51f42ef28448258af3e2f769c0c06457b
Jose Copeti <jrcopeti@gmail.com>
2024-10-09 14:31:27 +0200

    sign in

--- src/features/authentication/authApi.ts
+++ src/features/authentication/authApi.ts
@@ -1,5 +1,16 @@
 import EncryptedStorage from 'react-native-encrypted-storage';
 import {lofftApi} from 'reduxFeatures/api/lofftApi';
+import {LOFFT_API_CLIENT_SECRET, LOFFT_API_CLIENT_ID} from '@env';
+
+type SignInArgs = {
+  email: string;
+  password: string;
+};
+type SignInBody = SignInArgs & {
+  client_id: string;
+  client_secret: string;
+  grant_type: string;
+};
 
 export const authApi = lofftApi.injectEndpoints({
   endpoints: builder => ({
@@ -14,7 +25,20 @@ export const authApi = lofftApi.injectEndpoints({
       },
     }),
 
-    // signIn: builder.mutation<void, {email: string; password: string}
+    signIn: builder.mutation<void, SignInArgs>({
+      query: ({email, password}) => ({
+        url: 'authentication/signIn',
+        method: 'POST',
+        body: {
+          email,
+          password,
+          client_id: LOFFT_API_CLIENT_ID,
+          client_secret: 'LOFFT_API_CLIENT_SECRET',
+          grant_type: 'password',
+        } as SignInBody,
+      }),
+      invalidatesTags: [{type: 'User', id: 'PROFILE'}],
+    }),
   }),
   overrideExisting: false,
 });
