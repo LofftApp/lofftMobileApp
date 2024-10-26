3fb3ec5943e69010e88fdbad2e3e45b2b3bce92d
Jose Copeti <jrcopeti@gmail.com>
2024-10-26 02:44:38 +0200

    refactored footer styles in new user journey

--- src/screens/registration/lessor/FlatDetailsScreen.tsx
+++ src/screens/registration/lessor/FlatDetailsScreen.tsx
@@ -199,10 +199,7 @@ const styles = StyleSheet.create({
 
   inputText: {},
   footerContainer: {
-    flex: 1,
     paddingTop: _size(20),
-    paddingBottom: _size(20),
-    paddingHorizontal: _size(0),
     gap: _size(10),
   },
 });
