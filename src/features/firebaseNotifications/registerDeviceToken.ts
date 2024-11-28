6b3dc39c5bbfcb7485ac60fb717e69b14f0f0469
Jose Copeti <jrcopeti@gmail.com>
2024-11-28 20:57:16 +0100

    removed google services from tracking

--- src/features/firebaseNotifications/registerDeviceToken.ts
+++ src/features/firebaseNotifications/registerDeviceToken.ts
@@ -1,14 +1,17 @@
 import messaging from '@react-native-firebase/messaging';
 import {MutationTrigger} from '@reduxjs/toolkit/dist/query/react/buildHooks';
 import {MutationDefinition} from '@reduxjs/toolkit/query';
+import {Platform} from 'react-native';
 
 export type RegisterTokenType = MutationTrigger<
   MutationDefinition<string, any, any, {message: string}, 'lofftApi'>
 >;
 export const registerDeviceToken = async (registerToken: RegisterTokenType) => {
   try {
-    // Register the device with FCM
-    await messaging().registerDeviceForRemoteMessages();
+    // Register the device with FCM (Android only)
+    if (Platform.OS === 'android') {
+      await messaging().registerDeviceForRemoteMessages();
+    }
 
     // Get the FCM token
     const token = await messaging().getToken();
