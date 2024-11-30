578fdfa3b196e03d12f2432f1a5826738530e9d4
Jose Copeti <jrcopeti@gmail.com>
2024-11-30 23:29:03 +0100

    included get notifications when receiving a foregorund notification

--- src/hooks/useForegroundNotifications.ts
+++ src/hooks/useForegroundNotifications.ts
@@ -1,17 +1,21 @@
 import {useEffect} from 'react';
 import messaging from '@react-native-firebase/messaging';
 import {handleForegroundNotifications} from 'reduxFeatures/firebaseNotifications/handleForegroundNotifications';
+import {useAppDispatch} from 'reduxCore/hooks';
+import {fcmApi} from 'reduxFeatures/firebaseNotifications/fcmApi';
 
 export const useForegroundNotifications = (isAuth: boolean) => {
+  const dispatch = useAppDispatch();
   useEffect(() => {
     if (!isAuth) {
       return;
     }
 
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      await handleForegroundNotifications(remoteMessage);
+    const unsubscribe = messaging().onMessage(remoteMessage => {
+      handleForegroundNotifications(remoteMessage);
+      dispatch(fcmApi.endpoints.getNotifications.initiate());
     });
 
     return () => unsubscribe();
-  }, [isAuth]);
+  }, [isAuth, dispatch]);
 };
