71ad97209e68b66d861f3b297d4050fe6027f82e
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2024-12-18 15:44:46 +0100

    types for getChatrooms improved due to new data coming in

--- src/features/chatrooms/types.ts
+++ src/features/chatrooms/types.ts
@@ -1,5 +1,16 @@
+interface Message {
+  id: number;
+  chatroom_id: number;
+  user_id: number;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  read: boolean;
+}
+
 interface Chatroom {
   id: number;
+  message: Message
 }
 
 interface ChatroomsState {
