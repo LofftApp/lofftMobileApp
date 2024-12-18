7207573b272feb5befe95ed5716ad81067e6b2e1
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2024-12-18 12:32:10 +0100

    created LessorChatIndexScreen

---
+++ src/screens/dashboard/landlord/LessorChatIndexScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LessorChatIndexScreen = () => {
+  return(
+    <View>
+      <Text>Hello from LessorChatIndexScreen</Text>
+    </View>
+  );
+};
+
+export default LessorChatIndexScreen;
