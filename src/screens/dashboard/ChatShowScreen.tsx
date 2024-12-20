443ef197ed20a87391603f24a3b111242b06c332
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2024-12-20 17:04:21 +0100

    created & centralized ChatShowScreen

---
+++ src/screens/dashboard/ChatShowScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const ChatShowScreen = () => {
+  return(
+    <View>
+      <Text>Hello from ChatShowScreen</Text>
+    </View>
+  );
+};
+
+export default ChatShowScreen;
